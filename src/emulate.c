732f6f0c2d93c17f03882aead03087bd6ccb8732
dg1814 <dg1814@ic.ac.uk>
2015-05-22 14:39:37 +0100

    Parsing of bin files implemented (sort of).

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,41 @@
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+void parseInstructions(char *dest, FILE *program) {
+  fseek(program, 0, SEEK_END);
+  const int programSize = ftell(program);
+  fseek(program, 0, SEEK_SET);
+
+  printf("%i\n",programSize);
+
+  dest = malloc( programSize * sizeof(char));
+
+  fread(dest, 4, programSize, program);
+
+  for(int i=0; i < programSize; i++) {
+    printf("%u\n", dest[i]);
+  }
+}
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+  printf("%i\n", argc);
+  assert (argc == 2);
+
+  const char *fileName = argv[1]; 
+ 
+  printf("%s\n", fileName);
+
+  FILE *program;
+  program  = fopen(fileName, "r");
+  assert (program);
+
+  char *opCodes = 0;
+
+  parseInstructions(opCodes, program);
+  
+  free(opCodes);
+  fclose(program);
+ 
+  return EXIT_SUCCESS; 
 }
