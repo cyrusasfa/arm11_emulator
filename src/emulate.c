cce563e4bb30af16c1c87ea724503375054db4d4
dg1814 <dg1814@ic.ac.uk>
2015-05-23 23:00:46 +0100

    Implemented the parsing with a global memory for the emulation

--- src/emulate.c
+++ src/emulate.c
@@ -2,40 +2,34 @@
 #include <assert.h>
 #include <stdio.h>
 
-void parseInstructions(char *dest, FILE *program) {
-  fseek(program, 0, SEEK_END);
-  const int programSize = ftell(program);
-  fseek(program, 0, SEEK_SET);
+const int memSize = 64 * 1024; 
+char *memory; //Array to represent the memory
 
-  printf("%i\n",programSize);
+int parseInstructions(FILE *program) {
 
-  dest = malloc( programSize * sizeof(char));
+  int ret = fread(memory, 1, memSize , program);
 
-  fread(dest, 4, programSize, program);
-
-  for(int i=0; i < programSize; i++) {
-    printf("%u\n", dest[i]);
+  for(int i=0; i < ret; i++) {
+    printf("%u\n", memory[i]);
   }
+
+  return ret;
 }
 
 int main(int argc, char **argv) {
   printf("%i\n", argc);
   assert (argc == 2);
-
   const char *fileName = argv[1]; 
- 
   printf("%s\n", fileName);
-
+  
+  memory = malloc(memSize * sizeof(char));
+  
   FILE *program;
   program  = fopen(fileName, "r");
   assert (program);
-
-  char *opCodes = 0;
-
-  parseInstructions(opCodes, program);
-  
-  free(opCodes);
+  parseInstructions(program);
   fclose(program);
- 
+  
+  free(memory); 
   return EXIT_SUCCESS; 
 }
