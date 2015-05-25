83b6cac5c2616fc287343b8654c77c4407eda7d3
Mohammad Karamlou <mak514@pixel38.doc.ic.ac.uk>
2015-05-25 22:42:50 +0100

    started the pipeline. Still has a lot of bugs

--- src/emulate.c
+++ src/emulate.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 
 #define MEM_SIZE (64*1024)
+#define NUM_REGS (17)
+#define REG_SIZE (32)
+
 #define r0 (registers[0])
 #define r1 (registers[1])
 #define r2 (registers[2])
@@ -22,11 +25,43 @@
 #define cpsr (registers[16])
 
 char *memory; //Array to represent the memory
-char **registers;
+int **registers; // represents the registers
+
+void initializeMemories();
+
+int parseInstructions(const char *filename);
+
+char* fetch(int address);
+
+char* decode(const char *fetched);
+
+void execute(const char *decoded);
+
+int main(int argc, char **argv) {
+  assert (argc == 2);
+  const char *fileName = argv[1]; 
+  
+  initializeMemories();
+  //Set all memory locations and registers to 0 
+  parseInstructions(fileName);
+  //Parse binary file and upload contents into memory
+  
+  char *fetched = NULL;
+  char *decoded = NULL;
+  
+  while (strcmp(decoded, "halt") != 0) {
+    // execute(decoded);
+    // decoded = decode(fetched);
+    // fetched = fetch(pc);
+    // pc += 4;    
+  }
+  
+  return EXIT_SUCCESS; 
+}
 
 void initializeMemories() {
   memory    = (char *) malloc(MEM_SIZE * sizeof(char));
-  registers = (char **) malloc(sizeof(char *) * 17);
+  registers = (int **) malloc(sizeof(int *) * NUM_REGS);
   //Allocate space for 17 reg pointers
   
   for (int i=0; i < MEM_SIZE; i++) {
@@ -34,13 +69,13 @@ void initializeMemories() {
   }
   //Set all memory locations to 0
   
-  for (int i=0; i < 17; i++) {
-    registers[i] = (char *) malloc(sizeof(char) * 32); 
+  for (int i=0; i < NUM_REGS; i++) {
+    registers[i] = (char *) malloc(sizeof(char) * REG_SIZE); 
   }
   //Allocate 32 "fake" bits per reg
   
-  for (int rNum=0; rNum < 17; rNum++) {
-    for (int bit=0; bit < 32; bit++) {
+  for (int rNum=0; rNum < NUM_REGS; rNum++) {
+    for (int bit=0; bit < REG_SIZE; bit++) {
       registers[rNum][bit] = 0;
     }
   }
@@ -62,14 +97,20 @@ int parseInstructions(const char *fileName) {
   return ret;
 }
 
-int main(int argc, char **argv) {
-  assert (argc == 2);
-  const char *fileName = argv[1]; 
-  
-  initializeMemories();
-  //Set all memory locations and registers to 0 
-  parseInstructions(fileName);
-  //Parse binary file and upload contents into memory
-  
-  return EXIT_SUCCESS; 
+char* fetch(int address) {
+  char* result;
+  memcpy(result, memory[address], 4);
+  return result;
+}
+
+char* decode(const char *fetched) {
+  if (!fetched) {
+    return NULL; 
+  }   
+}
+
+void execute(const char *decoded) {
+  if (!decoded) {
+    return;
+  }
 }
