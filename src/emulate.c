d516ef78667493fa64ebf61bea8b50ec2aaf3c12
Mihai Barbu <mihai.barbu14@imperial.ac.uk>
2015-05-26 20:26:16 +0100

    started dataprocessing

--- src/emulate.c
+++ src/emulate.c
@@ -1,114 +1,91 @@
-#include <stdlib.h>
-#include <assert.h>
 #include <stdio.h>
-#include <stdint.h>
 
-#define MEM_SIZE (64*1024)
-#define NUM_REGS (17)
-#define REG_SIZE (32)
+struct registers {
+	int r_0;  // general purpose
+	int r_1;  // general purpose
+	int r_2;  // general purpose
+	int r_3;  // general purpose
+	int r_4;  // general purpose
+	int r_5;  // general purpose
+	int r_6;  // general purpose
+	int r_7;  // general purpose
+	int r_8;  // general purpose
+	int r_9;  // general purpose
+	int r_10; // general purpose
+	int r_11; // general purpose
+	int r_12; // general purpose
+	int r_13; // SP
+	int r_14; // LR
+	int r_15; // PC
+	int r_16; // CPSR
+};
 
-#define r0 (registers[0])
-#define r1 (registers[1])
-#define r2 (registers[2])
-#define r3 (registers[3])
-#define r4 (registers[4])
-#define r5 (registers[5])
-#define r6 (registers[6])
-#define r7 (registers[7])
-#define r8 (registers[8])
-#define r9 (registers[9])
-#define r10 (registers[10])
-#define r11 (registers[11])
-#define r12 (registers[12])
-#define r13 (registers[13])
-#define r14 (registers[14])
-#define pc  (registers[15])
-#define cpsr (registers[16])
+char memory[64 * 1024]; // 1 char = 1 byte 
+int registers[17]; 
 
-uint8_t *memory; //Array to represent the memory
-uint32_t registers[NUM_REGS]; // represents the registers
+void dataprocessing(int);
 
-void initializeMemories();
+int main(){
 
-int loadMemory(const char *filename);
 
-uint32_t fetch(int address); 
-
-void* decode(uint8_t fetched[4]);
-
-void execute(void *decoded);
-
-int main(int argc, char **argv) {
-  assert (argc == 2);
-  const char *fileName = argv[1]; 
-  
-  initializeMemories();
-    //Set all memory locations and registers to 0 
-  loadMemory(fileName);
-    //Parse binary file and upload contents into memory
-  
-  uint32_t fetched;
-  //void *decoded = NULL;
-  
-  //while () {
-    // execute(decoded);
-    // decoded = decode(fetched);
-    // fetched = fetch(pc);
-    // pc += 4;    
-  //}
-
-  fetched = fetch(0);
-  printf("%u", fetched);
-  
-  free(memory); 
-    
-  return EXIT_SUCCESS; 
+	return 0;
 }
 
-void initializeMemories() {
-  memory    = (uint8_t *) malloc(MEM_SIZE * sizeof(uint8_t));
-  
-  for (int i=0; i < MEM_SIZE; i++) {
-    memory[i] = 0;
-  }
-  //Set all memory locations to 0
-  
-  for (int i=0; i < NUM_REGS; i++) {
-    registers[i] = 0; 
-  }
-  //Set all registers to 0
+void dataprocessing(int instruction){
+	int o2;
+	int bitS     = 20;
+	int bitI     = 25;
+	int immstrt  = 0;
+	int immlgth  = 8;
+	int rotstrt  = 8;
+	int rotlngth = 4;
+    int rmstrt   = 0;
+    int rmlngth  = 4;
+	bool flagS   = false;
+	if (readbit(instruction, bitS)) {
+		flagS = true;
+		printf("Set flag S");
+	}
+	if (readbit(instruction, bitI)){ // I = 1 if operand 2 is an immediat value
+        printf("I = 1\n");
+		o2 = takeint(instruction)
+		unsigned int imm    = takeint(instruction, imm, immlgth);
+		unsigned int rotate = takeint(instruction, rotstrt, rotlngth);
+		rotate <<= 2;
+		o2 = rotatateright(imm, rotate);
+		printf("o2 = %d\n", o2);
+
+	} else { // I = 0  we shift register
+        printf("I = 0\n");
+        int rm = takeint(instruction, rmstrt, rmlngth);
+        
+    }
+
 }
 
-int loadMemory(const char *fileName) {
-
-  FILE *program;
-  program  = fopen(fileName, "rb");
-  
-  assert (program);
-  int ret = fread(memory, 1, MEM_SIZE, program);
-
-  fclose(program);
-  return ret;
+bool readbit(int instruction, int index) { //reading start from right to left. begin with index 0
+	return takeint(instrc, index, 1) == 1;
 }
 
-uint32_t fetch(int address) {
-  uint32_t ret = 0;
-  for(int i=3; i > 0; --i) {
-    ret += memory[address+i];
-    ret = ret << 8;
+int takeint(int instruction, int start, int length) {      // start is the start bit from where we extract the bits 
+  if (start > 31 || start < 0) {                           // length is how many bits we want to extract
+	  printf("%s\n", "take int error");                    // instructionm is from where we want to extract
+	  printf("start = %d, length = %d\n", start, length);
+	  return 0;
+  } else {
+      instruction >>= start;
+      int mask = (1 << length) - 1;
+      instruction = mask & instruction;     
   }
-  return ret += memory[address];
 }
 
-void* decode(uint8_t fetched[4]) {
-  if (!fetched) {
-    return NULL; 
-  }
-  return NULL; 
-}
-
-void execute(void *decoded) {
-  if (!decoded) {
-    return;
-  }
+int rotatateright(int x, int y) {
+   if (y != 0) {
+	   //carryout = readbit(x, y-1);
+	   int firstYbits = x << (32 - y);
+	   int lastbits = x >> y;
+	   retun (firstYbits | lastbits);
+   } else {
+	  return x;
+   }
 }
