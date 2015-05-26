8b19ad4a5fb17865836e6bd6c7983d8b4acb699b
Mihai Barbu <mihai.barbu14@imperial.ac.uk>
2015-05-26 21:28:04 +0100

    code style improvement

--- src/emulate.c
+++ src/emulate.c
@@ -42,7 +42,7 @@ void dataprocessing(int);
 
 bool readbit(int instruction, int index);
 
-int takeint(int instruction, int start, int length);
+int extractbits(int instruction, int start, int length);
 
 int rotatateright(int x, int y);
 
@@ -74,14 +74,14 @@ int main(int argc, char **argv) {
 }
 
 void initializeMemories() {
-  memory    = (uint8_t *) malloc(MEM_SIZE * sizeof(uint8_t));
+  memory = (uint8_t *) malloc(MEM_SIZE * sizeof(uint8_t));
   
-  for (int i=0; i < MEM_SIZE; i++) {
+  for (int i = 0; i < MEM_SIZE; i++) {
     memory[i] = 0;
   }
   //Set all memory locations to 0
   
-  for (int i=0; i < NUM_REGS; i++) {
+  for (int i = 0; i < NUM_REGS; i++) {
     registers[i] = 0; 
   }
   //Set all registers to 0
@@ -90,7 +90,7 @@ void initializeMemories() {
 int loadMemory(const char *fileName) {
 
   FILE *program;
-  program  = fopen(fileName, "rb");
+  program = fopen(fileName, "rb");
   
   assert (program);
   int ret = fread(memory, 1, MEM_SIZE, program);
@@ -101,7 +101,7 @@ int loadMemory(const char *fileName) {
 
 uint32_t fetch(int address) {
   uint32_t ret = 0;
-  for(int i=3; i > 0; --i) {
+  for(int i = 3; i > 0; --i) {
     ret += memory[address+i];
     ret = ret << 8;
   }
@@ -116,46 +116,54 @@ void decodeAndExecute(uint32_t fetched) {
 }
 
 void dataprocessing(int instruction){
-	int o2;
-	int bitS     = 20;
-	int bitI     = 25;
-	int immstrt  = 0;
-	int immlgth  = 8;
-	int rotstrt  = 8;
-	int rotlngth = 4;
-    int rmstrt   = 0;
-    int rmlngth  = 4;
-	bool flagS   = false;
-	if (readbit(instruction, bitS)) {
-		flagS = true;
-		printf("Set flag S");
-	}
-	if (readbit(instruction, bitI)){ // I = 1 if operand 2 is an immediat value
+  int o2;
+  int bitS         = 20;
+  int bitI         = 25;
+  int immstrt      = 0;
+  int immlgth      = 8;
+  int rotstrt      = 8;
+  int rotlngth     = 4;
+  int rmstrt       = 0;
+  int rmlngth      = 4;
+  int operand2bit4 = 4;
+  bool flagS       = false;
+  if (readbit(instruction, bitS)) {
+    flagS = true;
+    printf("Set flag S");
+  }
+  if (readbit(instruction, bitI)){ // I = 1 if operand 2 is an immediat value
         printf("I = 1\n");
-		o2 = takeint(instruction)
-		unsigned int imm    = takeint(instruction, imm, immlgth);
-		unsigned int rotate = takeint(instruction, rotstrt, rotlngth);
-		rotate <<= 2;
-		o2 = rotatateright(imm, rotate);
-		printf("o2 = %d\n", o2);
+    o2                  = extractbits(instruction)
+    unsigned int imm    = extractbits(instruction, imm, immlgth);
+    unsigned int rotate = extractbits(instruction, rotstrt, rotlngth);
+    rotate <<= 2;
+    o2                  = rotatateright(imm, rotate);
+    printf("o2 = %d\n", o2);
 
-	} else { // I = 0  we shift register
+  } else { // I = 0  we shift register
         printf("I = 0\n");
-        int rm = takeint(instruction, rmstrt, rmlngth);
-        
+        int rm         = extractbits(instruction, rmstrt, rmlngth);
+        int valrm      = registers[rm]; // value of register
+        int shifttype  = extractbits(instruction, 5, 2);
+        int shiftvalue = 0;
+        if (readbit(instruction, operand2bit4)){ // if bit 4 is 1
+            int rs = extractbits(instrc, 8, 4);
+            int vrs = reg[rs];
+        }
+
     }
 
 }
 
 //reading start from right to left. begin with index 0
 bool readbit(int instruction, int index) { 
-	return takeint(instrc, index, 1) == 1;
+	return extractbits(instrc, index, 1) == 1;
 }
 
 // start is the start bit from where we extract the bits
 // length is how many bits we want to extract
 // instructionm is from where we want to extract
-int takeint(int instruction, int start, int length) {       
+int extractbits(int instruction, int start, int length) {       
   if (start > 31 || start < 0) {                           
 	  printf("%s\n", "take int error");                   
 	  printf("start = %d, length = %d\n", start, length);
@@ -177,3 +185,4 @@ int rotatateright(int x, int y) {
 	  return x;
    }
 }
+
