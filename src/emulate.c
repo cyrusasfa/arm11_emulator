3bb33b4f84fbdd9139f4ec3f68e076f6154b4a0d
Mohammad Karamlou <mak514@corona15.doc.ic.ac.uk>
2015-05-26 21:07:05 +0100

    fixed the merging issues

--- src/emulate.c
+++ src/emulate.c
@@ -34,9 +34,17 @@ int loadMemory(const char *filename);
 
 uint32_t fetch(int address); 
 
-void* decode(uint8_t fetched[4]);
+void decodeAndExecute(uint32_t fetched);
 
 void execute(void *decoded);
+
+void dataprocessing(int);
+
+bool readbit(int instruction, int index);
+
+int takeint(int instruction, int start, int length);
+
+int rotatateright(int x, int y);
 
 int main(int argc, char **argv) {
   assert (argc == 2);
@@ -100,15 +108,72 @@ uint32_t fetch(int address) {
   return ret += memory[address];
 }
 
-void* decode(uint8_t fetched[4]) {
+void decodeAndExecute(uint32_t fetched) {
   if (!fetched) {
     return NULL; 
   }
   return NULL; 
 }
 
-void execute(void *decoded) {
-  if (!decoded) {
-    return;
+void dataprocessing(int instruction){
+	int o2;
+	int bitS     = 20;
+	int bitI     = 25;
+	int immstrt  = 0;
+	int immlgth  = 8;
+	int rotstrt  = 8;
+	int rotlngth = 4;
+    int rmstrt   = 0;
+    int rmlngth  = 4;
+	bool flagS   = false;
+	if (readbit(instruction, bitS)) {
+		flagS = true;
+		printf("Set flag S");
+	}
+	if (readbit(instruction, bitI)){ // I = 1 if operand 2 is an immediat value
+        printf("I = 1\n");
+		o2 = takeint(instruction)
+		unsigned int imm    = takeint(instruction, imm, immlgth);
+		unsigned int rotate = takeint(instruction, rotstrt, rotlngth);
+		rotate <<= 2;
+		o2 = rotatateright(imm, rotate);
+		printf("o2 = %d\n", o2);
+
+	} else { // I = 0  we shift register
+        printf("I = 0\n");
+        int rm = takeint(instruction, rmstrt, rmlngth);
+        
+    }
+
+}
+
+//reading start from right to left. begin with index 0
+bool readbit(int instruction, int index) { 
+	return takeint(instrc, index, 1) == 1;
+}
+
+// start is the start bit from where we extract the bits
+// length is how many bits we want to extract
+// instructionm is from where we want to extract
+int takeint(int instruction, int start, int length) {       
+  if (start > 31 || start < 0) {                           
+	  printf("%s\n", "take int error");                   
+	  printf("start = %d, length = %d\n", start, length);
+	  return 0;
+  } else {
+      instruction >>= start;
+      int mask = (1 << length) - 1;
+      instruction = mask & instruction;     
   }
+}
+
+int rotatateright(int x, int y) {
+   if (y != 0) {
+	   //carryout = readbit(x, y-1);
+	   int firstYbits = x << (32 - y);
+	   int lastbits = x >> y;
+	   retun (firstYbits | lastbits);
+   } else {
+	  return x;
+   }
 }
