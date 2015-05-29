ad388d4fe34913943d9f9fdbd38f1d01516e0d0b
Mohammad Karamlou <mak514@ray22.doc.ic.ac.uk>
2015-05-29 14:14:57 +0100

    started assembler. Can write ints to output file one at a time

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,31 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv) {
+  assert(argc == 3);
+  
+  const char *srcname = argv[1];
+  const char* dstname = argv[2];
+  
+  FILE *src;
+  FILE * dst;
+  
+  src = fopen(srcname, "r");
+  dst = fopen(dstname, "wb");
+  
+  assert (src != NULL);
+  assert (dst != NULL);
+      
+  fclose(src);
+  fclose(dst);
+  
   return EXIT_SUCCESS;
+}
+
+//writes an int (instruction) to the binary file;
+void writeToOutput(FILE *dst, int x) {
+  fwrite(&x, sizeof(x), 1, dst);
 }
