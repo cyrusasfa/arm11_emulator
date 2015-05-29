892f1bfc62d6c47bf9ff7510b4865f4c465848a5
Mohammad Karamlou <mak514@ray22.doc.ic.ac.uk>
2015-05-29 15:35:52 +0100

    started the assembler. Can write binary files. Tokeniser is not finished

--- src/assemble.c
+++ src/assemble.c
@@ -3,6 +3,20 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define MAX_LENGTH (512)
+struct Tokenised {
+   char* label;
+   char* opcode;
+   char* operands[];
+};
+
+void writeToOutput(FILE *dst, int x);
+
+void tokenise(struct Tokenised parsedinput[], FILE* src);
+
+int getnumlines(FILE* src);
 
 int main(int argc, char **argv) {
   assert(argc == 3);
@@ -18,7 +32,12 @@ int main(int argc, char **argv) {
   
   assert (src != NULL);
   assert (dst != NULL);
+  
+  struct Tokenised parsedinput[getnumlines(src)];      
+  tokenise(parsedinput, src);
+    
       
+  
   fclose(src);
   fclose(dst);
   
@@ -28,4 +47,20 @@ int main(int argc, char **argv) {
 //writes an int (instruction) to the binary file;
 void writeToOutput(FILE *dst, int x) {
   fwrite(&x, sizeof(x), 1, dst);
+}
+
+int getnumlines(FILE* src) {
+  int num = 0;
+  char str[MAX_LENGTH];
+  while (fgets(str, MAX_LENGTH, src)) {
+    num++;
+  }
+  return num;
+}
+void tokenise(struct Tokenised parsedinput[], FILE* src) {
+  char line[MAX_LENGTH];
+  for (int i = 0; fgets(line, MAX_LENGTH, src); i++) {
+    parsedinput[i].label = strtok(line, ":");
+    printf("%s\n", parsedinput[i].label);
+  }
 }
