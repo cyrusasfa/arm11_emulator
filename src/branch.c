408aab2a13197392f50dd02500d3c1fc3a24ad03
dg1814 <dg1814@ic.ac.uk>
2015-06-03 00:30:30 +0100

    Corrected branch logic and cpsr setting logic, still need to fix the str and the ldr instructions amongst other things, but progress is being made

--- src/branch.c
+++ src/branch.c
@@ -16,8 +16,14 @@ void get_arg(unsigned int offset, struct pipeline *pip) {
 }
 
 void branch(uint32_t *arg, struct machine_state *mach, struct pipeline *pip) {
-  mach->registers[15] += arg[0];
-  //printf("Got to here branch function");
+  uint32_t *args = (uint32_t*) malloc (4 * sizeof(uint32_t));
+  //printf("WEIRD ASS OFFSET : %i\n", *arg);
+  *(args) = 15;
+  *(args+1) =  *arg;
+  *(args+2) = 15;
+  *(args+3) = 0;
+  add(args, mach, pip);
+  //printf("THE PC IS %i\n", mach->registers[15]);
   pip->fetched = 0;
   pip->halt = 0;
   pip->decoded = &do_nothing;
@@ -29,9 +35,15 @@ void decode_branch(uint32_t instr, struct pipeline *pip, struct machine_state
                     *mach) {
   //printf("Got to decode branch");
   unsigned int offset = extract_bits(instr, 0, 24);
-  offset <<= 2;
-  uint32_t move = offset;
-  
+  uint32_t move = offset << 2;
+  int sign_bit = read_bit(move, 25); // After left shift of 2 sign bit is 26
+  //printf(" THE SIGN BIT : %i\n", sign_bit); 
+  if (sign_bit) {
+    for(int i = 26; i < 32; i++) {
+      move = set_bit(move, i);
+    }
+  }
+
   get_arg(move, pip);
   pip->decoded = &branch; 
 }
