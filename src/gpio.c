bfa9d3eb318bbc8c7a3940de0e5b0239adb1d285
Mihai  Barbu <mihai.barbu14@imperial.ac.uk>
2015-06-09 21:55:33 +0100

    debug gpio.c

--- src/gpio.c
+++ src/gpio.c
@@ -5,9 +5,10 @@
 #include <string.h>
 #include "utility.h"
 #include "gpio.h"
+#include <unistd.h>
 
 void gpio_run (uint32_t memory_out, uint32_t memory_clear, uint32_t memory_set, int gpio_pin) {
-	if(extract_bits(memory, 18, 3) != 1) {
+  if(extract_bits(memory_out, 18, 3) != 1) {
 		set_pin(memory_out, 18);
 		clear_pin(memory_out, 19);
 		clear_pin(memory_out, 20);
